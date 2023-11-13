45b696d197a10c600fe798f2c9aa2b13154ea78d
nenadmne <cosovicnenad14@gmail.com>
2023-11-13 18:18:30 +0100

    Adding loading and error state to data fetching utility

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { type ReactNode, useEffect, useState } from "react";
 import get from "./util/http";
 import BlogPosts, { BlogPost } from "./components/BlogPosts";
 import fetchingImage from "../src/assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage";
 
 type rawBlogPosts = {
   id: number;
@@ -10,31 +11,52 @@ type rawBlogPosts = {
   body: string;
 };
 function App() {
-  const [posts, setPosts] = useState<BlogPost[]>([]);
+  const [posts, setPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
 
   const getPosts = async () => {
-    const data = (await get(
-      "https://jsonplaceholder.typicode.com/posts"
-    )) as rawBlogPosts[];
+    setIsFetching(true);
+    try {
+      const data = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as rawBlogPosts[];
 
-    const blogData: BlogPost[] = data.map((item) => {
-      return {
-        id: item.id,
-        title: item.title,
-        text: item.body,
-      };
-    });
+      const blogData: BlogPost[] = data.map((item) => {
+        return {
+          id: item.id,
+          title: item.title,
+          text: item.body,
+        };
+      });
+      setPosts(blogData);
+    } catch (error) {
+      setError((error as Error).message);
+    }
 
-    setPosts(blogData);
+    setIsFetching(false);
   };
   useEffect(() => {
     getPosts();
   }, []);
 
+  let content: ReactNode;
+
+  if (error) {
+    content = <ErrorMessage text={error} />;
+  }
+
+  if (posts) {
+    content = <BlogPosts posts={posts} />;
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback"> Fetching posts... </p>;
+  }
   return (
     <main>
       <img src={fetchingImage} alt="fetch image" />
-      <BlogPosts posts={posts} />
+      {content}
     </main>
   );
 }
